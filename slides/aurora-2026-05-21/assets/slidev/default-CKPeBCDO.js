ed6c2d3a0bf1c94dce18c40180e8752566d549a6
Barış Deniz Sağlam <bdsaglam@gmail.com>
2026-05-21 00:44:08 +0300

    Update Aurora slides — link OPSD tutorial

---
+++ slides/aurora-2026-05-21/assets/slidev/default-CKPeBCDO.js
@@ -0,0 +1,1 @@
+import{H as e,b as t,z as n}from"../modules/shiki-DgM9Q0bi.js";import{vt as r}from"../index-CL6B75jM.js";var i={},a={class:`slidev-layout default`};function o(r,i){return n(),t(`div`,a,[e(r.$slots,`default`)])}var s=r(i,[[`render`,o]]);export{s as t};
