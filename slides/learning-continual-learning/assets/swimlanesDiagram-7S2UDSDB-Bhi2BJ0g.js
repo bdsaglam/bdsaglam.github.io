edeb21175888f7d86598fcbf3c251190d16e340b
Barış Deniz Sağlam <bdsaglam@gmail.com>
2026-07-21 02:14:20 +0300

    slides: learning continual learning position pitch

---
+++ slides/learning-continual-learning/assets/swimlanesDiagram-7S2UDSDB-Bhi2BJ0g.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-QQNUYIFB-jsuB1N-g.js";import{c as a}from"./Mermaid.vue_vue_type_script_setup_true_lang-1AYPyETu.js";import"./chunk-QVUG6NDL-CygxJLkI.js";import"./chunk-RTBOCTTP-ZxqUvNhg.js";import"./chunk-HPLX5OYV-BFeqjvEQ.js";import"./chunk-DRARJEGU-CaEwN0ZL.js";import"./modules/vue-C-lC1Vdf.js";import"./index-CXMSRpIw.js";import"./modules/shiki-CD056Xai.js";import"./modules/file-saver-B7oFTzqn.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
