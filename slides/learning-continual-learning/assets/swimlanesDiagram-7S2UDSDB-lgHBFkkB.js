d94234db73b206bab0dd78995ff3b04ba44b40e0
Barış Deniz Sağlam <bdsaglam@gmail.com>
2026-07-21 12:30:28 +0300

    slides: stop calling lambda a sentence where it means the whole learning act

---
+++ slides/learning-continual-learning/assets/swimlanesDiagram-7S2UDSDB-lgHBFkkB.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-QQNUYIFB-Ddok4KZ2.js";import{c as a}from"./Mermaid.vue_vue_type_script_setup_true_lang-C5kxLnaI.js";import"./chunk-QVUG6NDL-CTd5sTCh.js";import"./chunk-RTBOCTTP-nFb0F5JM.js";import"./chunk-HPLX5OYV-CAdqJLxC.js";import"./chunk-DRARJEGU-B8-h5BmX.js";import"./modules/vue-Da-TM9fM.js";import"./index-Bwo0Csws.js";import"./modules/shiki-DULBhgOe.js";import"./modules/file-saver-B7oFTzqn.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
