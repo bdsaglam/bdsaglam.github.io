58eba58caa25e5156fbc7eaf6df31958af515e74
Barış Deniz Sağlam <bdsaglam@gmail.com>
2026-07-21 12:10:20 +0300

    slides: name the method Language of Learning (LoL); add mini deck for the research group

--- slides/learning-continual-learning/assets/swimlanesDiagram-7S2UDSDB-BDody_lr.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-QQNUYIFB-Am3nu280.js";import{c as a}from"./Mermaid.vue_vue_type_script_setup_true_lang-CKhjfzPo.js";import"./chunk-QVUG6NDL-BDZBLKll.js";import"./chunk-RTBOCTTP-DxXMTbrd.js";import"./chunk-HPLX5OYV-CXQOelta.js";import"./chunk-DRARJEGU-DpwCZMkg.js";import"./modules/vue-C-lC1Vdf.js";import"./index-p1pH3zLs.js";import"./modules/shiki-CD056Xai.js";import"./modules/file-saver-B7oFTzqn.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
---
+++ slides/learning-continual-learning/assets/swimlanesDiagram-7S2UDSDB-BFz-h_Tx.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-QQNUYIFB-WSNyTWHJ.js";import{c as a}from"./Mermaid.vue_vue_type_script_setup_true_lang-Cjnf0j4A.js";import"./chunk-QVUG6NDL-C4gM1_Re.js";import"./chunk-RTBOCTTP-CBoIe_2C.js";import"./chunk-HPLX5OYV-DSPxXwy_.js";import"./chunk-DRARJEGU-CDDC0ZPl.js";import"./modules/vue-C-lC1Vdf.js";import"./index-Chhi51nz.js";import"./modules/shiki-CD056Xai.js";import"./modules/file-saver-B7oFTzqn.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
