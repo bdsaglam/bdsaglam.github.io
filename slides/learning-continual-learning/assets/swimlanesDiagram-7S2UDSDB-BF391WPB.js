c134b318c228aefaa3aa746a287c0bfdd0ee69de
Barış Deniz Sağlam <bdsaglam@gmail.com>
2026-07-21 12:24:57 +0300

    slides: clearer kill-tests slide

--- slides/learning-continual-learning/assets/swimlanesDiagram-7S2UDSDB-BF391WPB.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-QQNUYIFB-DXmI77EW.js";import{c as a}from"./Mermaid.vue_vue_type_script_setup_true_lang-BIwPftnS.js";import"./chunk-QVUG6NDL-BI0NDvIE.js";import"./chunk-RTBOCTTP-BNu5aSic.js";import"./chunk-HPLX5OYV-DeSRKR8b.js";import"./chunk-DRARJEGU-BC5WtR8v.js";import"./modules/vue-Da-TM9fM.js";import"./index-BiNvV-RE.js";import"./modules/shiki-DULBhgOe.js";import"./modules/file-saver-B7oFTzqn.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
