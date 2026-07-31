c7ea558df36027e16fe99fc111b143e7fe412e82
Barış Deniz Sağlam <bdsaglam@gmail.com>
2026-07-31 11:48:43 +0300

    slides: SEAL also appears under the language pillar (explicit act path)

---
+++ slides/learning-continual-learning/assets/swimlanesDiagram-7S2UDSDB-BUdCN3sc.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-QQNUYIFB-CdWbVdF1.js";import{c as a}from"./Mermaid.vue_vue_type_script_setup_true_lang-DDTU_2Sn.js";import"./chunk-QVUG6NDL-D7UwDmR-.js";import"./chunk-RTBOCTTP-z3e9CTyf.js";import"./chunk-HPLX5OYV-C-xk8rYT.js";import"./chunk-DRARJEGU-oj1Kndqe.js";import"./modules/vue-C-lC1Vdf.js";import"./index-WS5EFgHr.js";import"./modules/shiki-CD056Xai.js";import"./modules/file-saver-B7oFTzqn.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
